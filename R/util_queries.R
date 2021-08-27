88cccf29a4d961034225a3989ad3c4472137c473
eric-hunt <52219938+eric-hunt@users.noreply.github.com>
2021-08-27 13:41:42 -0400

    updated product query for alphabetical sorting

--- R/util_queries.R
+++ R/util_queries.R
@@ -37,7 +37,8 @@ sql_queries$products <- {
   FROM products p
   WHERE EXISTS (SELECT *
                 FROM uncle_experiment_sets exp_sets
-                WHERE exp_sets.product_id = p.id)"
+                WHERE exp_sets.product_id = p.id)
+  ORDER BY p.name"
 }
 
 
--- sql/product_list.sql
+++ sql/product_list.sql
@@ -1,7 +1,8 @@
--- !preview conn=ebase_ro
+-- !preview conn=ebase_test
 
 SELECT p.name AS product_name, p.id AS product_id, p.catalog_number
 FROM products p
 WHERE EXISTS (SELECT *
               FROM uncle_experiment_sets exp_sets
               WHERE exp_sets.product_id = p.id)
+ORDER BY p.name
