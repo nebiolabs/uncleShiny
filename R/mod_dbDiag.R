3fd36cdedb28e10120fddd3b2a2fcd61ddd29497
eric-hunt <52219938+eric-hunt@users.noreply.github.com>
2021-08-27 09:15:16 -0400

    added filters and fixed queries in db diagnostics
    Filters for product_id and experiment_set_id were added.
    
    Queries were edited to use left joins instead of inner
    joins, which were dropping rows when tables were NULL.

--- R/mod_dbDiag.R
+++ R/mod_dbDiag.R
@@ -34,8 +34,9 @@ dbDiagUI <- function(id) {
         ),
         shiny::br(),
         shiny::br(),
-        shiny::textOutput(
-          ns("dbobj_print")
+        shiny::actionButton(
+          ns("bttn_run_all"),
+          "Run All Diag Queries"
         ),
         shiny::br(),
         shiny::br(),
@@ -45,12 +46,16 @@ dbDiagUI <- function(id) {
           choices = palChoices,
           selected = "Spectral"
         ),
-        shiny::br(),
         shiny::selectInput(
           ns("palette2"),
           "Palette 2:",
           choices = palChoices,
           selected = "Set2"
+        ),
+        shiny::br(),
+        "Connection object:",
+        shiny::verbatimTextOutput(
+          ns("dbobj_print")
         )
       ),
       ##----------------------------------------
@@ -65,6 +70,12 @@ dbDiagUI <- function(id) {
             shiny::actionButton(
               ns("bttn_get_products"),
               "Retrieve Products"
+            ),
+            shiny::br(),
+            shiny::numericInput(
+              ns("filter_products"),
+              "Filter product_id:",
+              value = NA
             )
           ),
           shiny::column(
@@ -73,6 +84,12 @@ dbDiagUI <- function(id) {
             shiny::actionButton(
               ns("bttn_get_exp_sets"),
               "Retrieve All Sets"
+            ),
+            shiny::br(),
+            shiny::numericInput(
+              ns("filter_exp_sets"),
+              "Filter exp_set_id:",
+              value = NA
             )
           ),
           shiny::column(
@@ -130,40 +147,106 @@ dbDiagServer <- function(id, dbobj) {
       
       
       ##-----------------------------------------
-      ##  Products with exp_set n              --
+      ##  Queries                              --
       ##-----------------------------------------
-      # Query
-      diag_products <- shiny::eventReactive(input$bttn_get_products, {
-        if (db_is_valid()) {
-          DBI::dbGetQuery(
-            dbobj,
-            glue::glue_sql(
-              "SELECT p.name, COUNT(exp_sets.id) AS n_exp_sets,
-                p.id AS product_id
-              FROM products p
-              INNER JOIN uncle_experiment_sets exp_sets
-                ON exp_sets.product_id = p.id
-              GROUP BY p.id
-              ORDER BY name",
-              .con = dbobj  
-            )
-          )
-        }
-      })
-      # Output
-      output$products <- DT::renderDT({
+      query_products <- {
+        "SELECT p.name, COUNT(exp_sets.id) AS n_exp_sets,
+          p.id AS product_id
+        FROM products p
+        INNER JOIN uncle_experiment_sets exp_sets
+          ON exp_sets.product_id = p.id
+        GROUP BY p.id
+        ORDER BY product_id"
+      }
+        
+      query_exp_sets <- {
+        "SELECT p.id AS product_id,
+          CONCAT(exp_sets.exp_type, '_', exp_sets.plate_generation) AS plate, 
+          COUNT(exps.id) AS n_exps, exp_sets.id AS exp_set_id
+        FROM products AS p
+        INNER JOIN uncle_experiment_sets AS exp_sets
+          ON exp_sets.product_id = p.id
+        LEFT JOIN uncle_experiments AS exps
+          ON exps.uncle_experiment_set_id = exp_sets.id
+        GROUP BY p.id, exp_sets.id
+        ORDER BY exp_set_id"
+      }
+        
+      query_exps <- {
+        "SELECT p.id AS product_id, exp_sets.id AS exp_set_id,
+          exps.plate_side AS side, exps.uncle_instrument_id AS inst_id,
+        exps.date, COUNT(sums.id) AS n_sums, exps.id AS exp_id
+        FROM products AS p
+        INNER JOIN uncle_experiment_sets AS exp_sets
+          ON exp_sets.product_id = p.id
+        LEFT JOIN uncle_experiments AS exps
+          ON exps.uncle_experiment_set_id = exp_sets.id
+        LEFT JOIN uncle_summaries AS sums
+          ON sums.uncle_experiment_id = exps.id
+        GROUP BY p.id, exp_sets.id, exps.id
+        ORDER BY exp_set_id"
+      }
+      
+      
+      ##----------------------------------------
+      ##  Helper functions                    --
+      ##----------------------------------------
+      myDT <- function(df, hide_cols = NULL) {
         DT::datatable(
-          data = diag_products(),
+          data = df,
           selection = "none",
           rownames = FALSE,
           options = list(
             dom = "it",
             scrollX = FALSE,
-            scrollY = "1000px",
+            scrollY = FALSE,
             paging = FALSE,
-            scrollCollapse = TRUE
+            scrollCollapse = TRUE,
+            columnDefs = list(
+              list(
+                visible = FALSE,
+                targets = hide_cols
+              )
+            )
           )
-        ) |> 
+        )
+      }
+      
+
+      ##-----------------------------------------
+      ##  Products with exp_set n              --
+      ##-----------------------------------------
+      # Query
+      diag_products <- shiny::eventReactive(
+        c(input$bttn_get_products, input$bttn_run_all),
+        ignoreInit = TRUE, {
+        if (db_is_valid()) {
+          DBI::dbGetQuery(
+            dbobj,
+            glue::glue_sql(
+              query_products,
+              .con = dbobj  
+            )
+          )
+        }
+      })
+      
+      # Filter
+      product_filter <- shiny::debounce(shiny::reactive({
+        if (is.na(input$filter_products)) {
+          return(unique(diag_products()$product_id))
+        } else {
+          return(input$filter_products)
+        }
+      }), 2000)
+      
+      # Output
+      output$products <- DT::renderDT({
+        shiny::req(diag_products(), product_filter())
+        df <- diag_products() |> 
+          dplyr::filter(product_id %in% product_filter())
+        
+        myDT(df) |> 
           DT::formatStyle(
             "product_id",
             backgroundColor = DT::styleEqual(
@@ -182,40 +265,39 @@ dbDiagServer <- function(id, dbobj) {
       ##  Exp_sets with exps n                --
       ##----------------------------------------
       # Query
-      diag_exp_sets <- shiny::eventReactive(input$bttn_get_exp_sets, {
+      diag_exp_sets <- shiny::eventReactive(
+        c(input$bttn_get_exp_sets, input$bttn_run_all),
+        ignoreInit = TRUE, {
         if (db_is_valid()) {
           DBI::dbGetQuery(
             dbobj,
             glue::glue_sql(
-              "SELECT p.id AS product_id,
-                CONCAT(exp_sets.exp_type, '_', exp_sets.plate_generation) AS plate, 
-                COUNT(exps.id) AS n_exps, exp_sets.id AS exp_set_id
-              FROM products AS p
-              INNER JOIN uncle_experiment_sets AS exp_sets
-                ON exp_sets.product_id = p.id
-              INNER JOIN uncle_experiments AS exps
-                ON exps.uncle_experiment_set_id = exp_sets.id
-              GROUP BY p.id, exp_sets.id
-              ORDER BY exp_set_id",
+              query_exp_sets,
               .con = dbobj  
             )
           )
         }
       })
+      
+      # Filter
+      exp_set_filter <- shiny::debounce(shiny::reactive({
+        if (is.na(input$filter_exp_sets)) {
+          return(unique(diag_exp_sets()$exp_set_id))
+        } else {
+          return(input$filter_exp_sets)
+        }
+      }), 2000)
+      
       # Output
       output$exp_sets <- DT::renderDT({
-        DT::datatable(
-          data = diag_exp_sets(),
-          selection = "none",
-          rownames = FALSE,
-          options = list(
-            dom = "it",
-            scrollX = FALSE,
-            scrollY = "1000px",
-            paging = FALSE,
-            scrollCollapse = TRUE
+        shiny::req(diag_exp_sets(), product_filter(), exp_set_filter())
+        df <- diag_exp_sets() |> 
+          dplyr::filter(
+            product_id %in% product_filter(),
+            exp_set_id %in% exp_set_filter()
           )
-        ) |> 
+        
+        myDT(df) |> 
           DT::formatStyle(
             columns = "product_id",
             backgroundColor = DT::styleEqual(
@@ -256,40 +338,30 @@ dbDiagServer <- function(id, dbobj) {
       ##  Exps with summary n                  --
       ##-----------------------------------------
       # Query
-      diag_exps <- shiny::eventReactive(input$bttn_get_exps, {
+      diag_exps <- shiny::eventReactive(
+        c(input$bttn_get_exps, input$bttn_run_all),
+        ignoreInit = TRUE, {
         if (db_is_valid()) {
           DBI::dbGetQuery(
             dbobj,
             glue::glue_sql(
-              "SELECT exp_sets.id AS exp_set_id,
-                exps.plate_side AS side, exps.uncle_instrument_id AS inst_id,
-              exps.date, COUNT(sums.id) AS n_sums, exps.id AS exp_id
-              FROM uncle_experiment_sets AS exp_sets
-              INNER JOIN uncle_experiments AS exps
-                ON exps.uncle_experiment_set_id = exp_sets.id
-              INNER JOIN uncle_summaries AS sums
-                ON sums.uncle_experiment_id = exps.id
-              GROUP BY exp_sets.id, exps.id
-              ORDER BY exp_set_id",
+              query_exps,
               .con = dbobj  
             )
           )
         }
       })
+      
       # Output
       output$exps <- DT::renderDT({
-        DT::datatable(
-          data = diag_exps(),
-          selection = "none",
-          rownames = FALSE,
-          options = list(
-            dom = "it",
-            scrollX = FALSE,
-            scrollY = "1000px",
-            paging = FALSE,
-            scrollCollapse = TRUE
+        shiny::req(diag_exps(), product_filter(), exp_set_filter())
+        df <- diag_exps() |> 
+          dplyr::filter(
+            product_id %in% product_filter(),
+            exp_set_id %in% exp_set_filter()
           )
-        ) |>  
+        
+        myDT(df, c(0)) |>  
           DT::formatStyle(
             columns = "exp_set_id",
             backgroundColor = DT::styleEqual(
@@ -315,7 +387,7 @@ dbDiagServer <- function(id, dbobj) {
           DT::formatStyle(
             columns = "n_sums",
             backgroundColor = DT::styleInterval(
-              c(47,49), c("#67a9cf90", NA, "#ef8a6290")
+              c(1,47,49), c("#ffffbf90", "#67a9cf90", "#a1d76a90", "#ef8a6290")
             )
           )
       })
