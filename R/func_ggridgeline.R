6e83879521466f80521afc55323959a7957222a1
eric-hunt <52219938+eric-hunt@users.noreply.github.com>
2021-10-08 10:58:33 -0400

    added variable switches

--- R/func_ggridgeline.R
+++ R/func_ggridgeline.R
@@ -13,4 +13,36 @@ ggridgeline <- function(data, spec_type, dls_type = "intensity",
   if (!(dls_type %in% c("intensity", "mass"))) {
     stop("Warning: invalid dls_type; use 'intensity' or 'mass'.")
   }
+  
+  ##----------------------------------------
+  ##  Variable switching                  --
+  ##----------------------------------------
+  spec_switch <- list(
+    "dsf" = "specTm",
+    "sls" = list("specSLS266", "specSLS473"),
+    "dls" = list("specDLS_I", "specDLS_M"),
+    "corr" = "specDLS_C"
+  )
+  x_switch <- list(
+    "dsf" = "temperature",
+    "sls" = "temperature",
+    "dls" = "hydrodynamic_diameter",
+    "corr" = "time"
+  )
+  y_switch <- list(
+    "dsf" = "bcm",
+    "sls" = list("sls_266", "sls_473"),
+    "dls" = "amplitude",
+    "corr" = "amplitude"
+  )
+  summary_switch <- list(
+    "dsf" = "Tm1",
+    "sls" = list("Tagg266", "Tagg473"),
+    "dls" = "Z_D",
+    "corr" = NA_character_
+  )
+  spec_var <- do.call(switch, c(spec_type, spec_switch))
+  x_var <- do.call(switch, c(spec_type, x_switch))
+  y_var <- do.call(switch, c(spec_type, y_switch))
+  summary_var <- do.call(switch, c(spec_type, summary_switch))
 }
