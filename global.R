e8206e0d923d315ed732019ce30997d2655dd9be
eric-hunt <52219938+eric-hunt@users.noreply.github.com>
2021-10-12 13:58:54 -0400

    switched to production mode

--- global.R
+++ global.R
@@ -46,8 +46,7 @@ library(glue)
 ##----------------------------------------
 ##  Testing flag                        --
 ##----------------------------------------
-use_testing_mode <- TRUE
-# TODO change back to FALSE before merge
+use_testing_mode <- FALSE
 
 if (use_testing_mode) {
   source("R/util_normalizeSpectra.R")
