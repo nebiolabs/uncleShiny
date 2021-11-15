91dec21327c140b38b405116e8a4f873bba73098
eric-hunt <52219938+eric-hunt@users.noreply.github.com>
2021-11-15 09:11:53 -0500

    applied updated queries to the app

--- sql/conditions_units.sql
+++ sql/conditions_units.sql
@@ -1,22 +1,32 @@
--- !preview conn=ebase_test
+-- !preview conn=ebase_dev
 
 WITH cte_sum AS
-(SELECT wells.layout_address AS well, sum.* 
-FROM uncle_summaries sum
-INNER JOIN wells
-  ON sum.well_id = wells.id
-WHERE EXISTS (SELECT *
-              FROM uncle_experiments exps
-              WHERE exps.uncle_experiment_set_id IN (47)
-                AND sum.uncle_experiment_id = exps.id)
-)
-SELECT exp_conds.well_id, exp_conds.raw_value AS unit_value, units.name AS unit_name,
-  conds.name AS condition_name, conds.type AS condition_type
+    (SELECT wells.layout_address AS well, sums.* 
+    FROM uncle_summaries sums
+    INNER JOIN wells
+      ON sums.well_id = wells.id
+    WHERE EXISTS (SELECT *
+                  FROM uncle_experiments exps
+                  WHERE exps.uncle_experiment_set_id IN (33, 32)
+                    AND sums.uncle_experiment_id = exps.id
+                  )
+    )
+SELECT exp_conds.well_id, exp_conds.raw_value AS unit_value,
+  units.name AS unit_name,
+  conds.name AS condition_name, conds.id AS condition_id,
+  grps.name AS group_name, grps.id AS group_id,
+  grpings.included, grpings.id AS grouping_id
 FROM experimental_conditions AS exp_conds
 INNER JOIN units
-  ON exp_conds.unit_id = units.id
+ ON exp_conds.unit_id = units.id
 INNER JOIN conditions AS conds
   ON exp_conds.condition_id = conds.id
+INNER JOIN condition_groupings AS grpings
+  ON grpings.condition_id = conds.id
+INNER JOIN condition_groups AS grps
+  ON grps.id = grpings.condition_group_id
 WHERE EXISTS (SELECT *
               FROM cte_sum
-              WHERE cte_sum.well_id = exp_conds.well_id)
+              WHERE cte_sum.well_id = exp_conds.well_id
+              )
+--ORDER BY exp_conds.well_id
