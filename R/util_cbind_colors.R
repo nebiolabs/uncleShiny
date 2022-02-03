75829a48a2803f756f1c653c4375649f50e1e06d
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2022-02-02 20:41:58 -0500

    cleaned up function for encoding color hex values

--- R/util_cbind_colors.R
+++ R/util_cbind_colors.R
@@ -7,30 +7,7 @@ cbind_colors <- function(df, color_var, palette_name) {
   the_length <- length(unique(df[[color_var]]))
   cat(paste0("The length of the palette is ", the_length,".\n"))
   
-  # if (is.null(df)) stop("There's a cbind problem.")
-  # 
-  # print(df)
-  # readr::write_rds(df, "test/cbind_output.rds")
-  
-  # character_conversions <- c(
-  #   "exp_id",
-  #   "exp_set_id",
-  #   "instrument",
-  #   "well_id"
-  # )
-  
   df |> 
-    # dplyr::mutate(
-    #   # convert 64-bit integers to character strings before factoring
-    #   dplyr::across(
-    #     .cols = tidyselect::any_of(character_conversions),
-    #     .fns = ~ ifelse(
-    #       bit64::is.integer64(.x),
-    #       bit64::as.character.integer64(.x),
-    #       .x
-    #     )
-    #   )
-    # ) |> 
     dplyr::mutate(
       !!color_var := forcats::fct_infreq(.data[[color_var]])
     ) |> 
