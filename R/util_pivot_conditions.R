23c7e637979b31b087d038beed657dd3ca80508c
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2023-06-14 14:26:29 -0400

    Altered pivot function to accommodate multiple entries per group

--- R/util_pivot_conditions.R
+++ R/util_pivot_conditions.R
@@ -3,12 +3,24 @@
 ##  Pivot conditions and units                                            --
 ##--------------------------------------------------------------------------
 
-pivot_conditions <- function(df) {
-  tidyr::pivot_wider(
-    data = df[df$included == 1, ],
-    id_cols = well_id,
-    names_from = group_name,
-    values_from = c(condition_name, unit_value, unit_name),
-    names_glue = "{group_name}_{.value}"
-  )
+pivot_conditions <- function(df, ...) {
+  df |> 
+    dplyr::filter(included == TRUE) |> 
+    dplyr::select(!c(included, condition_id, grouping_id)) |> 
+    dplyr::mutate(
+      condition = glue::glue(
+        "{unit_value}{unit_name} {condition_name}"
+      ),
+      .keep = "unused"
+    ) |> 
+    dplyr::summarize(
+      condition = stringr::str_c(condition, collapse = ", "),
+      .by = c(well_id, group_id, group_name)
+    ) |> 
+    tidyr::pivot_wider(
+      id_cols = well_id,
+      names_from = group_name,
+      values_from = condition,
+      ...
+    )
 }
